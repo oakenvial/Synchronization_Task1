25a3bdc197d4e6dadb0a51a80cf6868f868138d2
Yulia Nikolaeva <julia.nikolaeva@live.com>
2024-07-16 20:37:52 +0300

    Add frequency log

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 
 public class Main {
+    public static boolean frequencyCalculated = true;
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
     public static void main(String[] args) throws InterruptedException {
         final int NUM_ROUTES = 1000;
@@ -11,6 +12,8 @@ public class Main {
         final int LENGTH = 100;
 
         List<Thread> threads = new ArrayList<>();
+        Thread freqlogThread = new Thread(Main::logHighestFrequency);
+        freqlogThread.start();
         for (int i = 0; i < NUM_ROUTES; i++) {
             Thread thread = new Thread(() -> generateRoute(LETTERS, LENGTH));
             threads.add(thread);
@@ -19,12 +22,31 @@ public class Main {
         for (Thread thread : threads) {
             thread.join();
         }
+        freqlogThread.interrupt();
 
-        int highestFrequencyKey = Collections.max(sizeToFreq.entrySet(), Map.Entry.comparingByValue()).getKey();
-        System.out.printf("Самое частое количество повторений %d (встретилось %d раз)\n", highestFrequencyKey, sizeToFreq.get(highestFrequencyKey));
+        //int highestFrequencyKey = Collections.max(sizeToFreq.entrySet(), Map.Entry.comparingByValue()).getKey();
+        //System.out.printf("Самое частое количество повторений %d (встретилось %d раз)\n", highestFrequencyKey, sizeToFreq.get(highestFrequencyKey));
         System.out.print("Другие размеры:\n");
         for (Integer key : sizeToFreq.keySet().stream().sorted().toList()) {
             System.out.printf("- %d (%d раз)\n", key, sizeToFreq.get(key));
+        }
+    }
+
+    public static void logHighestFrequency() {
+        synchronized (sizeToFreq) {
+            while (!Thread.interrupted()) {
+                while (frequencyCalculated) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                }
+                int highestFrequencyKey = Collections.max(sizeToFreq.entrySet(), Map.Entry.comparingByValue()).getKey();
+                System.out.printf("Самое частое количество повторений %d (встретилось %d раз)\n", highestFrequencyKey, sizeToFreq.get(highestFrequencyKey));
+                frequencyCalculated = true;
+                sizeToFreq.notifyAll();
+            }
         }
     }
 
@@ -40,7 +62,17 @@ public class Main {
         }
 
         synchronized (sizeToFreq) {
+            while (!frequencyCalculated) {
+                try {
+                    sizeToFreq.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            frequencyCalculated = false;
+            //System.out.println(".");
             sizeToFreq.put(countR, sizeToFreq.getOrDefault(countR, 0) + 1);
+            sizeToFreq.notifyAll();
         }
 
         return route.toString();
